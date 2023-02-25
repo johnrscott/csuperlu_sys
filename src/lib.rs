8df003937f236b211bcc8863f99d44754b68eff1
John Scott <johnrscott0@gmail.com>
2023-02-25 18:34:14 +0000

    Revert "Revert "Moved src to old_src, added build back and started trying to generate bindings with rust-bindgen""
    This reverts commit f9128e4c48c9104d30978c88922095741888a20a.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,5 @@
-//! The C module contains the low-level interface to the SuperLU library.
-//!
-//! All functions that interface directly with SuperLU have the same names
-//! as the corresponding SuperLU functions, with an additional c_ prepended
-//! to the name.
-//!
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
-pub mod comp_col;
-pub mod dense;
-pub mod expert_driver;
-pub mod options;
-pub mod simple_driver;
-pub mod stat;
-pub mod super_matrix;
-pub mod super_node;
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
